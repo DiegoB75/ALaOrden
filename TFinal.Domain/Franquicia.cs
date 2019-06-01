91ede87015d50dd3106b73ffe035510f0fc027b1
DiegoB <diego.leo994@gmail.com>
2019-06-01 06:18:55 -0500

    url names chages

--- TFinal.Domain/Franquicia.cs
+++ TFinal.Domain/Franquicia.cs
@@ -7,8 +7,8 @@ namespace TFinal.Domain
     {
         public int IdFranquicia { get; set; }
         public string Nombre { get; set; }
-        public string Url { get; set; }
-        public string Conexion { get; set; }
+        public string WebUrl { get; set; }
+        public string ApiUrl { get; set; }
         public string Logo { get; set; }
         public List<Sede> Sedes { get; set; }
         public List<ProductoFranquicia> ProductoFranquicias { get; set; }
