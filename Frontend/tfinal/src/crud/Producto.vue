83b98d77f06c5e738aa4739cedaaaeb6ddd27923
JALG99 <jorgelandaverry@hotmail.com>
2019-06-28 14:31:01 -0500

    CRUDProducto

--- Frontend/tfinal/src/crud/Producto.vue
+++ Frontend/tfinal/src/crud/Producto.vue
@@ -15,7 +15,7 @@
         ></v-text-field>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
-          <v-btn slot="activator" color="primary" dark class="mb-2">Nuevo</v-btn>
+          <v-btn slot="activator" color="primary" dark class="mb-2" @click.native="nuevoItem" >Nuevo</v-btn>
           <v-card>
             <v-card-title>
               <span class="headline">{{ formTitle }}</span>
@@ -25,10 +25,22 @@
               <v-container grid-list-md>
                 <v-layout wrap>
                   <v-flex xs12 sm12 md12>
-                    <v-text-field v-model="categoria" label="Categoria"></v-text-field>
+                    <v-autocomplete
+                      v-model="idCategoria"
+                      :items="categorias"
+                      item-text="nombre"
+                      item-value="idCategoria"
+                    >
+                    </v-autocomplete>
                   </v-flex>
                   <v-flex xs12 sm12 md12>
-                    <v-text-field v-model="marca" label="Marca"></v-text-field>
+                    <v-autocomplete
+                      v-model="idMarca"
+                      :items="marcas"
+                      item-text="nombre"
+                      item-value="idMarca"
+                    >
+                    </v-autocomplete>
                   </v-flex>
                   <v-flex xs12 sm12 md12>
                     <v-text-field v-model="nombre" label="Nombre"></v-text-field>
@@ -67,10 +79,10 @@
         <template slot="items" slot-scope="props">
           <td class="justify-center layout px-0">
             <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
-          
+            <v-icon small class="mr-2" @click="deleteItem(props.item)">delete</v-icon>
           </td>
-          <td>{{ props.item.categoria }}</td>
-          <td>{{ props.item.marca }}</td>
+          <td>{{ props.item.categoria.nombre }}</td>
+          <td>{{ props.item.marca.nombre }}</td>
           <td>{{ props.item.nombre }}</td>
           <td>{{ props.item.presentacion }}</td>
           <td>{{ props.item.cantidad }}</td>
@@ -92,6 +104,8 @@ export default {
   data() {
     return {
       productos: [],
+      categorias: [],
+      marcas: [],
       dialog: false,
       headers: [
         { text: "Opciones", value: "opciones", sortable: false },
@@ -109,8 +123,16 @@ export default {
       editedIndex: -1,
 
       //TODO:Model
-      
-
+      idProducto:"",
+      categoria:"",
+      marca:"",
+      nombre:"",
+      presentacion:"",
+      cantidad:"",
+      magnitud:"",
+      unidad:"",
+      descripcion:"",
+      imagen:"",
     };
   },
   computed: {
@@ -127,22 +149,67 @@ export default {
 
   created() {
     //TODO
-
+    this.listar();
   },
   methods: {
     listar() {
       //TODO
+      let me = this;
+
+      axios
+        .get("api/producto")
+        .then(function(response) {
+          //console.log(response);
+          me.productos = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
     editItem(item) {
       //TODO
+      this.idProducto = item.idProducto;
+      this.categoria = item.categoria;
+      this.marca = item.marca;
+      this.nombre = item.nombre;
+      this.presentacion = item.presentacion;
+      this.cantidad = item.cantidad;
+      this.magnitud = item.magnitud;
+      this.unidad = item.unidad;
+      this.descripcion = item.descripcion;
+      this.imagen = item.imagen;
+      this.dialog = true;
+      this.editedIndex = 1;
     },
     close() {
       this.dialog = false;
     },
+    nuevoItem(){
+      let me = this;
+      this.editedIndex = -1;
+      axios
+        .get("api/categoria")
+        .then(function(response) {
+          //console.log(response);
+          me.categorias = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      axios
+        .get("api/marca")
+        .then(function(response) {
+          //console.log(response);
+          me.marcas = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
     limpiar() {
-      this.id = "";
-      this.categoria = "";
-      this.marca = "";
+      this.idProducto = "";
+      this.idCategoria = "";
+      this.idMarca = "";
       this.nombre = "";
       this.presentacion = "";
       this.cantidad = "";
@@ -150,9 +217,73 @@ export default {
       this.unidad = "";
       this.descripcion = "";
       this.imagen = "";
+      this.editedIndex = -1;
+    },
+    deleteItem(item){
+        let me = this;
+
+        axios
+        .delete("api/producto/"+item.idProducto)
+        .then(function(response) {
+          //console.log(response);
+          me.close();
+          me.listar();
+          me.limpiar();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
     guardar() {
-     //TODO
+       if (this.editedIndex > -1) {
+        //Código para editar
+
+        let me = this;
+        axios 
+          .put("api/producto/"+me.idProducto, {
+            idProducto: me.idProducto,
+            idCategoria:me.idCategoria,
+            idMarca:me.idMarca,
+            nombre:me.nombre,
+            presentacion:me.presentacion,
+            cantidad: me.cantidad,
+            magnitud:me.magnitud,
+            unidad:me.unidad,
+            descripcion:me.descripcion,
+            imagen:me.imagen
+          })
+          .then(function(response) {
+            me.close();
+            me.listar();
+            me.limpiar();
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      } else {
+        //Código para guardar
+        let me = this;
+        axios
+          .post("api/producto", {
+            idCategoria:me.idCategoria,
+            idMarca:me.idMarca,
+            nombre:me.nombre,
+            presentacion:me.presentacion,
+            cantidad: me.cantidad,
+            magnitud:me.magnitud,
+            unidad:me.unidad,
+            descripcion:me.descripcion,
+            imagen:me.imagen
+          })
+          .then(function(response) {
+            me.close();
+            me.listar();
+            me.limpiar();
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
     }
   }
 };
