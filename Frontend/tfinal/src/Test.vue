e99eca6ac1d6f601f0fd7e0f7a5954f5ea89c949
Diego B <u201616874@upc.edu.pe>
2019-06-29 00:28:33 -0500

    changes

---
+++ Frontend/tfinal/src/Test.vue
@@ -0,0 +1,9 @@
+<template>
+    <product-card/>
+</template>
+<script>
+  import ProductCard from "./components/catalog/ProductCard";
+  export default {
+    components: {ProductCard}
+  }
+</script>
--- Frontend/tfinal/src/components/cart/Cart.vue
+++ Frontend/tfinal/src/components/cart/Cart.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="text-xs-center">
-    <v-menu offset-y>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      offset-x
+    >
       <template v-slot:activator="{ on }">
         <v-btn
-           color="primary"
-           dark
-           v-on="on"
+          color="indigo"
+          dark
+          v-on="on"
         >
-          Dropdown
+         Carrito  - {{cart.length}} items
         </v-btn>
       </template>
-      <v-list>
-        <v-list-tile
-                v-for="(item, index) in items"
-                :key="index"
-                @click=""
-        >
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
+
+      <v-card>
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Carrito de Compras</v-list-tile-title>
+              <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-divider></v-divider>
+
+        <v-list>
+          <div  v-for="(cartitem, index) in cart" :key="`fruit-${index}`">
+            <cart-item :carrito = "cartitem"></cart-item>
+          </div>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn flat @click="menu = false">Cancel</v-btn>
+          <v-btn color="primary" flat @click="gaa">Save</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-menu>
   </div>
 </template>
